8471b7452a5214552ac496397394a964c98dceba
Rotich Davis <rotichdavissabila@gmail.com>
2025-11-06 21:03:42 +0300

    new file added

---
+++ week2.py
@@ -0,0 +1,12 @@
+# Assignment operator w/ string (=)
+greek_island = "Santorini"
+
+
+#Assignment operator w/ integer & tuple (+=)
+#Earth is 4.4 billion years old
+earth_age_bln = 4.4
+#universe is 14 billion years old
+universe_age_bln = 14
+#Adding 0.1 billion years to earth's age
+earth_age_bln += 0.1
+print (earth_age_bln)
